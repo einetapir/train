33a95ef6611ee9e8f80c2b5cc4b9fb9b31e33b24
einetapir <etienne.roseau@hetic.net>
2022-05-04 17:09:19 +0200

    load train.py and dockerfile

---
+++ train.py
@@ -0,0 +1,18 @@
+from sklearn.linear_model import LinearRegression
+from json import loads, dumps
+import pickle
+
+with open('/data/indata.json', 'r') as f:
+    content = f.read()
+    TRAIN_INPUT = loads(content)
+
+with open('/data/outdata.json', 'r') as f:
+    content = f.read()
+    TRAIN_OUTPUT = loads(content)
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+print(f'Respect + : {predictor.coef_}')
+with open('model/model.json', 'w') as f:
+    f.write(dumps(predictor.coef_.tolist()))
